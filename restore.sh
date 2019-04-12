c4f47262df141770d4927b4ffdfd4c66f452e561
vidura <vidurada@gmail.com>
2019-04-12 17:38:42 +0530

    final fix

--- restore.sh
+++ restore.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #install java, rundeck,rd and unzip
-cd ~
+cd /home/ec2-user/
 sudo yum install java-1.8.0-openjdk java-1.8.0-openjdk-devel -y
 sudo rpm -Uvh http://repo.rundeck.org/latest.rpm
 sudo yum install rundeck -y
@@ -15,13 +15,13 @@ sudo yum install unzip -y
 #config rd
 my_ip=$( curl http://checkip.amazonaws.com )
 echo $my_ip
-echo "export RD_CONF=/etc/rundeck/rd.conf" >> ~/envir.sh
-echo "export RD_URL=http://$my_ip:4440/api/30" >> ~/envir.sh
-echo "export RD_USER=admin" >> ~/envir.sh
-echo "export RD_PASSWORD=admin" >> ~/envir.sh
+echo "export RD_CONF=/etc/rundeck/rd.conf" >> /home/ec2-user/envir.sh
+echo "export RD_URL=http://$my_ip:4440/api/30" >> /home/ec2-user/envir.sh
+echo "export RD_USER=vidura.d" >> /home/ec2-user/envir.sh   #add rundeck username here
+echo "export RD_PASSWORD=vidura@93" >> /home/ec2-user/envir.sh  #add rundeck password here
 
-chmod +x ~/envir.sh
-source ~/envir.sh
+chmod +x  /home/ec2-user/envir.sh
+source /home/ec2-user/envir.sh
 
 #config rundeck
 sudo sed -i "s/localhost/$my_ip/g" /etc/rundeck/rundeck-config.properties
@@ -29,37 +29,37 @@ sudo sed -i "s/localhost/$my_ip/g" /etc/rundeck/framework.properties
 
 #install aws cli
 curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
-unzip ~/awscli-bundle.zip
+unzip /home/ec2-user/awscli-bundle.zip
 ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
 
 #download backup files from s3
-mkdir rundeckBackup
-aws s3 cp s3://ust-rundeck-backup/rundeckBackup ~/rundeckBackup --recursive
+mkdir /home/ec2-user/rundeckBackup
+aws s3 cp s3://viduras-test-bucket/rundeckBackup /home/ec2-user/rundeckBackup --recursive
 
 #restore folders
 for entry in "/home/ec2-user/rundeckBackup"/*; do
     if [[ -d $entry ]]; then
         bk_folder=$( basename $entry  )
         if [[ $bk_folder != 'Projects' ]]; then
-                sudo cp -r ~/rundeckBackup/$bk_folder /var/lib/rundeck/$bk_folder
+                sudo cp -r /home/ec2-user/rundeckBackup/$bk_folder /var/lib/rundeck/$bk_folder
         fi
     fi
 done
 
 #replace the realm.properties file
 sudo rm -rf /etc/rundeck/realm.properties
-sudo cp ~/rundeckBackup/realm.properties /etc/rundeck/realm.properties
+sudo cp /home/ec2-user/rundeckBackup/realm.properties /etc/rundeck/realm.properties
 
 sudo /etc/init.d/rundeckd start
 
 
 #restore jobs
-sleep 120s
+sleep 120
 for entry in "/home/ec2-user/rundeckBackup/Projects"/*; do
     if [[ -d $entry ]]; then
         project_name=$( basename $entry )
         rd projects create -p $project_name
-        rd jobs load -f rundeckBackup/Projects/$project_name/jobs.xml -p $project_name
+        rd jobs load -f  /home/ec2-user/rundeckBackup/Projects/$project_name/jobs.xml -p $project_name
     fi
 done
 
@@ -72,3 +72,10 @@ make
 make altinstall
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 sudo python get-pip.py
+
+cd /home/ec2-user/rundeckBackup/
+filename="requirements.txt"
+while read -r line; do
+    name="$line"
+    sudo pip install $line
+done < "$filename"
